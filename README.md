20ba2c4ad64bb3a2639cdacf6868fa05a0cd8624
Tetiana Sakun <>
2024-04-18 23:23:15 +0200

    Add functionality for bash and docker tasks

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # goit-cs-hw-02
+
+## The repository for the second Computer Systems homework
+
+The result of the docker-compose up command 
+![Alt text](<Screenshot 2024-04-18 at 22.54.15.png>)
---
+++ bash_task/hw.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+sites=("https://google.com" "https://facebook.com" "https://twitter.com")
+
+filename="website_status.log"
+
+#Clean log file
+> "$filename"
+
+for site in ${sites[@]}
+do
+    if curl -o /dev/null -s -L --head --request GET -w "%{http_code}\\n" "$site" | grep "200" > /dev/null
+    then
+        echo "$site is UP" >> $filename
+    else
+        echo "$site is DOWN" >> $filename
+    fi
+done
+
+echo "Result saved in $filename"
